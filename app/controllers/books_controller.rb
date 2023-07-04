6a5e66751781c97665c98e07d55bd55ffe44d312
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-04 10:24:43 +0000

    tag

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,14 +4,13 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-     @book_tags = @book.tags
     @book_comment = BookComment.new
   end
 
   def index
-    @taglist = Tag.all
     @books = Book.all
-    @book = current_user.books.new.
+    @book = Book.new
+    @taglist = Tag.all
   end
 
   def create
@@ -47,7 +46,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :star)
+    params.require(:book).permit(:title, :body, :star, :tag)
   end
 
   def ensure_correct_user
--- app/models/book.rb
+++ app/models/book.rb
@@ -14,22 +14,6 @@ class Book < ApplicationRecord
     favorites.where(user_id: user.id).exists?
   end
 
-  def save_tag(sent_tags)
-    current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
-    old_tags = current_tags - sent_tags
-    new_tags = sent_tags - current_tags
-
-    old_tags.each do |old|
-      self.book_tags.delete BookTag.find_by(tag_name: old)
-    end
-
-    new_tags.each do |new|
-      new_book_tag = BookTag.find_or_create_by(tag_name: new)
-      self.book_tags << new_book_tag
-    end
-  end
-end
-
   def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
@@ -42,4 +26,3 @@ end
     end
   end
 end
-
